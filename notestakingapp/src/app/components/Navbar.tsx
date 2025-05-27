073f5cac3b7c9a1c1d7b6a9fda426f608c69228f
RakkeshIT <rakkeshit@gmail.com>
2025-05-27 21:06:42 +0530

    Notes Taking App Using Next Js

--- notestakingapp/src/app/components/Navbar.tsx
+++ notestakingapp/src/app/components/Navbar.tsx
@@ -5,15 +5,15 @@ const Navbar = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
         <nav className={`${Styles.N_Container}`}>
-            <div className={`${Styles.Title}`}>
-                Notes Taking
+            <div>
+                <Link className={`${Styles.Title}`} href=''>Notes Taking</Link>
             </div>
 
             <ul className={`${Styles.Menus}`}>
                 <li><Link href='/'>Home</Link></li>
                 <li><Link href=''>About</Link></li>
-                <li><Link href='/client/login'>Login</Link></li>
-                <li><Link href='/client/register'>Register</Link></li>
+                <li><Link href='/client/login'>🤵 Login</Link></li>
+                <li><Link href='/client/register' >✨ Register</Link></li>
             </ul>
         </nav>
 
